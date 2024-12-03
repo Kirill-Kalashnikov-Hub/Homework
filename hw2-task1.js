5c4418019dc318acbbc0a9c694f880305ba53193
Кирилл <k76739340@gmail.com>
2024-12-03 21:41:10 +0300

    Задание 2

--- hw2-task1.js
+++ hw2-task1.js
@@ -1,5 +1,9 @@
 //Задание 1
-let a = 10;
-alert(a)
-a = 20;
-alert(a);
+// let a = 10;
+// alert(a)
+// a = 20;
+// alert(a);
+
+//Задание 2
+ let ageIphone = 2007;
+ alert('Год выпуска первого iPhone:' + ageIphone)
