cc93615e38c07b29ed8847a466288f2e8b033f69
Кирилл <k76739340@gmail.com>
2024-12-06 20:57:15 +0300

    Задание 2

--- hw3.js
+++ hw3.js
@@ -1,8 +1,16 @@
 //Задание 1
-let password = 'пароль';
-let userPassword = prompt('Введите пароль');
-if (userPassword === password) {    
-    alert('Пароль введен верно');
+// let password = 'пароль';
+// let userPassword = prompt('Введите пароль');
+// if (userPassword === password) {    
+//     alert('Пароль введен верно');
+// }else { 
+//     alert('Пароль введен неверно');
+// }
+
+//Задание 2
+let c = 8;
+if (c >= 0 && c <= 10) {
+    alert('Верно');
 }else { 
-    alert('Пароль введен неверно');
+    alert('Неверно');
 }
