820826e90f122bb711c4e1418277031e9318a796
Кирилл <k76739340@gmail.com>
2024-12-20 18:19:54 +0300

    убрал полосу прокрутки по горизонтали

--- hw4.js
+++ hw4.js
@@ -1,44 +1,44 @@
 //Задание 1
-for (let i = 0; i < 2; i++) { 
-    console.log('Привет!');
-}
+// for (let i = 0; i < 2; i++) { 
+//     console.log('Привет!');
+// }
 
-//Задание 2
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
-}
+// //Задание 2
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
 
-//Задание 3
-for (let i = 7; i <=22; i++) {
-    console.log(i);
-}
+// //Задание 3
+// for (let i = 7; i <=22; i++) {
+//     console.log(i);
+// }
 
-//Задание 4
-const obj = {
-    Kolya: '200',
-    Vasya: '300',
-    Petya: '400'
-}
+// //Задание 4
+// const obj = {
+//     Kolya: '200',
+//     Vasya: '300',
+//     Petya: '400'
+// }
 
-for (const key in obj) {
-console.log(`${key} - ${obj[key]} $`);
-}
+// for (const key in obj) {
+// console.log(`${key} - ${obj[key]} $`);
+// }
 
-// Задание 5
-let n = 1000;
-let num = 0;
-while (n >= 50) {
-    n /= 2;
-    console.log(n);
-    num++;
-}
+// // Задание 5
+// let n = 1000;
+// let num = 0;
+// while (n >= 50) {
+//     n /= 2;
+//     console.log(n);
+//     num++;
+// }
 
-console.log(`Количество циклов: ${num}`);
+// console.log(`Количество циклов: ${num}`);
 
-//Задание 6 
-let dayFriday = 3;
-let allDay = 31;
-for (let i = dayFriday; i <= allDay; i += 7) {
-    console.log(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчёт.`);
+// //Задание 6 
+// let dayFriday = 3;
+// let allDay = 31;
+// for (let i = dayFriday; i <= allDay; i += 7) {
+//     console.log(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчёт.`);
     
-}
+// }
