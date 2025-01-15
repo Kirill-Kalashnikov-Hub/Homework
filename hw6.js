c42e1f4e09bef10d1ab656a378261630714d8dae
Кирилл <k76739340@gmail.com>
2025-01-15 13:55:44 +0300

    задание 1

--- hw6.js
+++ hw6.js
@@ -1,1 +1,9 @@
 console.log('Задание 1');
+const myArray = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+  if (myArray[i] === 10) {
+    break; // Прерываем цикл, когда встречаем 10
+  }
+}
