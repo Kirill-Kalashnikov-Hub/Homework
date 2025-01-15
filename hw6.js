7dc2b9f878dca3d9c0d8b243ab081893f2f10fc3
Кирилл <k76739340@gmail.com>
2025-01-15 14:30:18 +0300

    задание 3

--- hw6.js
+++ hw6.js
@@ -8,15 +8,19 @@
 //   }
 // }
 
-console.log('Задание 2');
-const myArray = [1, 5, 4, 10, 0, 3];
-const indexValue  = 4;
+// console.log('Задание 2');
+// const myArray = [1, 5, 4, 10, 0, 3];
+// const indexValue  = 4;
+// const index = myArray.findIndex(element => element === indexValue);
 
-const index = myArray.findIndex(element => element === indexValue);
+// if (index !== -1) {
+//   console.log(`Индекс значения ${indexValue} в массиве: ${index}`);
+// } else {
+//   console.log(`Значение ${indexValue} не найдено в массиве`);
+// }
 
-if (index !== -1) {
-  console.log(`Индекс значения ${indexValue} в массиве: ${index}`);
-} else {
-  console.log(`Значение ${indexValue} не найдено в массиве`);
-}
+console.log('Задание 3');
+const myArray = [1, 3, 5, 10, 20];
+const result = myArray.join(' ');
+console.log(result);
 
