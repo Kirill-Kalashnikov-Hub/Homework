0fdb6a945c8b44fc5a15ae8a463583dde90143b7
Кирилл <k76739340@gmail.com>
2025-01-18 02:29:55 +0300

    Задание 2

--- hw8.js
+++ hw8.js
@@ -7,3 +7,35 @@ const people = [
   ];
   
   console.log(people.sort((a, b) => a.age - b.age));
+
+
+  console.log('Задание 2');
+  function isPositive(num) {
+    return num > 0;
+  }
+  
+  function isMale(person) {
+    return person.gender === 'male';
+  }
+  
+  function filter(arr, ruleFunction) {
+      const result = [];
+      for(let i = 0; i < arr.length; i++){
+          if(ruleFunction(arr[i])) {
+              result.push(arr[i]);
+          }
+      }
+      return result;
+  }
+  
+  
+  console.log(filter([3, -4, 1, 9], isPositive));
+  
+  const users = [
+      {name: 'Глеб', gender: 'male'},
+      {name: 'Анна', gender: 'female'},
+      {name: 'Олег', gender: 'male'},
+      {name: 'Оксана', gender: 'female'}
+  ];
+  
+  console.log(filter(people, isMale));
